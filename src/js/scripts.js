c33d69852ca19f1b076a48a4a0260fc7c4ed91a7
Yuriy Bondar <bondar.europe@gmail.com>
2020-04-27 13:05:40 +0300

    adds megamenu

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -71,6 +71,34 @@ $(document).ready(function () {
   $('.btn-toggle-mobile').click(function () {
     $('.portfolio-inside-list').toggleClass('show-list-text');
   });
+
+  /* mega-menu */
+
+  $('.menu > ul > li:has( > ul)').addClass('menu-dropdown-icon');
+  $('.menu > ul > li > ul:not(:has(ul))').addClass('normal-sub');
+  $('.menu > ul').before(
+    '<a href="#" class="menu-mobile open">&nbsp;</a><a href="#" class="menu-mobile close">&nbsp;</a>',
+  );
+  $('.menu > ul > li').hover(function (e) {
+    if ($(window).width() > 943) {
+      $(this).children('ul').stop(true, false).fadeToggle(150);
+      e.preventDefault();
+    }
+  });
+  $('.menu > ul > li').click(function () {
+    if ($(window).width() <= 943) {
+      $(this).children('ul').fadeToggle(150);
+    }
+  });
+  $('.menu-mobile').click(function (e) {
+    $('.menu > ul').toggleClass('show-on-mobile');
+    $('.close').toggleClass('showBtn');
+    e.preventDefault();
+  });
+  $(window).resize(function () {
+    $('.menu > ul > li').children('ul').hide();
+    $('.menu > ul').removeClass('show-on-mobile');
+  });
 });
 
 new WOW().init();
